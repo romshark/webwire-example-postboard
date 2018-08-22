a78e94d69a9f88ceb489a1e03bdb66812ad6e381
Roman Sharkov <roman.sharkov@qbeon.com>
2018-08-22 16:47:34 +0200

    Fix incorrect permissions on user profile
    Allow guests to access user profile details.

--- server/apisrv/modules/resolver/getUser.go
+++ server/apisrv/modules/resolver/getUser.go
@@ -2,7 +2,6 @@ package resolver
 
 import (
 	"github.com/qbeon/webwire-messenger/server/apisrv/api"
-	"github.com/qbeon/webwire-messenger/server/apisrv/modules/authorizer"
 	"github.com/qbeon/webwire-messenger/server/apisrv/sessinfo"
 )
 
@@ -11,16 +10,6 @@ func (rsv *resolver) GetUser(
 	session *sessinfo.SessionInfo,
 	params *api.GetUserParams,
 ) (interface{}, error) {
-	// Check authorization
-	if err := rsv.authorizer.MeetsEitherOf(
-		session,
-		authorizer.IsAuthenticated(
-			"only authenticated clients are allowed to retrieve a user profile",
-		),
-	); err != nil {
-		return nil, err
-	}
-
 	// Instruct the engine to find the user profile
 	profile, err := rsv.engine.GetUser(params.Ident)
 	if err != nil {
